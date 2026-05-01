2d046629d2b82b608c1c46bda843eaba072f5fd2
Lhilove <pelumiade92@gmail.com>
2026-05-01 14:11:11 +0100

    added functions

--- simple-data-store/main.go
+++ simple-data-store/main.go
@@ -5,47 +5,69 @@ import (
 	"strings"
 )
 
+type user struct {
+	firstName string
+	lastName  string
+	email     string
+	age       int
+	password  []string
+}
+
+var user1 = user{
+	firstName: "Jeff",
+	lastName:  "Alex",
+	email:     "jeff.alex@example.com",
+	age:       21,
+	password:  []string{"password123"},
+}
+
+var user2 = user{
+	firstName: "Tobi",
+	lastName:  "Alabi",
+	email:     "tobi.alabi@gmail.com",
+	age:       21,
+	password:  []string{"password001"},
+}
+
+var user3 = user{
+	firstName: "John",
+	lastName:  "Sanjay",
+	email:     "john.sanjay@apple.com",
+	age:       21,
+	password:  []string{"password202"},
+}
+
 func main() {
-	type user struct {
-		firstName string
-		lastName  string
-		email     string
-		age       int
-		password  []string
-	}
 
-	user1 := user{
-		"Jeff",
-		"Alex",
-		"jeff.alex@example.com",
-		21,
-		[]string{"password123"},
-	}
-	// fmt.Printf("%+v\n", user1)
+	users := createUsers()
+	printUsers(users)
+	filtered := filterByDomain(users, "@apple.com")
+	fmt.Println("\nApple employees:")
+	printUsers(filtered)
+}
 
-	user2 := user{
-		"Tobi",
-		"Alabi",
-		"tobi.alabi@gmail.com",
-		21,
-		[]string{"password001"},
-	}
-	// fmt.Printf("%+v\n", user2)
-
-	user3 := user{
-		"John",
-		"Sanjay",
-		"john.sanjay@apple.com",
-		21,
-		[]string{"password202"},
-	}
-	// fmt.Printf("%+v\n", user3)
+func createUsers() []user {
 	users := []user{user1, user2, user3}
+	return users
+}
 
-	for index, value := range users {
-		fmt.Printf("index: %d, value: %v\n", index, value)
-		if strings.Contains(value.email, "@apple.com") {
-			fmt.Printf("user with email %s is an apple employee \n", value.email)
+func printUsers(users []user) {
+	for _, value := range users {
+		fmt.Printf("Name: %s %s, Email: %s, Age: %d\n", value.firstName, value.lastName, value.email, value.age)
+	}
+}
+
+func filterByDomain(users []user, domain string) []user {
+	var filtered []user
+	for _, value := range users {
+		if strings.Contains(value.email, domain) {
+			filtered = append(filtered, value)
 		}
+	}
+	return filtered
+}
+func printAppleUsers(users []user) {
+	for _, value := range users {
+		fmt.Printf("user with email %s is an apple employee\n", value.email)
 	}
 }
